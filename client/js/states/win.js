d58dffaf8fdf0c5547a172e12261531cfe25da55
darrenyu888 <darrenyu888@users.noreply.github.com>
2026-02-06 11:41:45 +0000

    ux(mobile): make win screen tap-to-return + auto-return

--- client/js/states/win.js
+++ client/js/states/win.js
@@ -7,6 +7,9 @@ class Win extends Phaser.State {
   }
 
   create() {
+    // Hide touch overlay on end screen
+    try { if (window.UK01Touch && window.UK01Touch.hide) window.UK01Touch.hide(); } catch (_) {}
+
     new Text({
       game: this.game,
       x: this.game.world.centerX,
@@ -17,6 +20,21 @@ class Win extends Phaser.State {
         fill: '#FFFFFF'
       }
     })
+
+    // Mobile-friendly: tap anywhere to return
+    try {
+      this.game.input.onTap.addOnce(() => this.returnToMenu());
+    } catch (_) {}
+
+    // Also allow pointer click
+    try {
+      this.game.input.onDown.addOnce(() => this.returnToMenu());
+    } catch (_) {}
+
+    // Auto return after a short delay (avoid getting stuck)
+    try {
+      this.game.time.events.add(3500, () => this.returnToMenu());
+    } catch (_) {}
   }
 
   update() {
@@ -31,10 +49,10 @@ class Win extends Phaser.State {
 
   winnerText() {
     if (this.skin) {
-      return `Player: "${this.skin}" won! Press Enter to return to main menu.`
+      return `Player: "${this.skin}" won! Tap to return to main menu.`
     }
 
-    return 'Opponent left! Press Enter to return to main menu.'
+    return 'Opponent left! Tap to return to main menu.'
   }
 }
 
